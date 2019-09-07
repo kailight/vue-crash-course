b7877e36f264e5972a2abca831d4fe8831cd0ca2
KaiLight <lastgrunger@gmail.com>
2019-09-08 02:05:22 +0300

    Lesson #11 Start

--- save.bat
+++
@@ -1,6 +0,0 @@
-SET VER=11.5
-SET MSG=Lesson #11 Test Result
-
-git tag -d %VER%
-git commit . -m "%MSG%"
-git tag -a %VER% -m "%MSG%"
---
+++ save.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git tag -d 11.5
+git add .
+git commit . -m "Lesson #11 Test Result"
+git tag -a 11.5 -m "Lesson #11 Test Result"
