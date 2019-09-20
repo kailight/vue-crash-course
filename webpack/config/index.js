ba25677a7eedd777e2af7f444455c1248d57f4cb
KaiLight <lastgrunger@gmail.com>
2019-09-20 23:15:33 +0300

    release branch

--- webpack/config/index.js
+++ webpack/config/index.js
@@ -14,8 +14,8 @@ module.exports = {
 
     // Various Dev Server settings
     // host: 'localhost', // can be overwritten by process.env.HOST
-    host: '0.0.0.0',
-    port: 4003, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: process.env.HOST,
+    port: process.env.PORT, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
