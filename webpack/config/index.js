d138a5e60d0a9801f27fc412cf4a5caca5ea0149
KaiLight <lastgrunger@gmail.com>
2019-09-20 23:30:07 +0300

    added procfile

--- webpack/config/index.js
+++ webpack/config/index.js
@@ -15,7 +15,8 @@ module.exports = {
     // Various Dev Server settings
     // host: 'localhost', // can be overwritten by process.env.HOST
     host: process.env.HOST,
-    port: process.env.PORT, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    // port : 4003, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: process.env.PORT,
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
