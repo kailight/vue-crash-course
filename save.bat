a30d6f4239193edf50db80175ffcfa1dc3402062
KaiLight <lastgrunger@gmail.com>
2019-09-21 22:53:21 +0300

    Lesson #4 Start

---
+++ save.bat
@@ -0,0 +1,4 @@
+git tag -d 4.5
+git add .
+git commit . -m "Lesson #4 Test Result"
+git tag -a 4.5 -m "Lesson #4 Test Result"
---
+++ save.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git tag -d 4.5
+git add .
+git commit . -m "Lesson #4 Test Result"
+git tag -a 4.5 -m "Lesson #4 Test Result"
