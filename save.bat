97fbf34076ea3d7c08de9d4ce9f53099e5de0045
KaiLight <lastgrunger@gmail.com>
2019-09-22 06:19:27 +0300

    Lesson #6 Start

--- save.bat
+++ save.bat
@@ -2,3 +2,4 @@ git tag -d 6.5
 git add .
 git commit . -m "Lesson #6 Test Result"
 git tag -a 6.5 -m "Lesson #6 Test Result"
+git push -f --tags origin master
--- save.sh
+++ save.sh
@@ -4,3 +4,5 @@ git tag -d 6.5
 git add .
 git commit . -m "Lesson #6 Test Result"
 git tag -a 6.5 -m "Lesson #6 Test Result"
+git push -f --tags origin master
+
