12153ff84ceba17448149552ff757516263f79ef
KaiLight <lastgrunger@gmail.com>
2019-09-22 07:17:48 +0300

    Lesson #3 Start

--- app/App.vue
+++ app/App.vue
@@ -1,94 +1,22 @@
 <template>
 <div id="app">
-  <div class="welcome wrapper">
-    <div class="welcome_image"></div>
-    <div class="welcome_message">Hello, I am {{Admin.name}}!</div>
-  </div>
-  <div class="form wrapper">
-    <div class="question">What is your name?</div>
-    <input class="username" v-model="User.name" type="text"/>
-    <button>✓</button>
-    <!--<div class="debug"><strong>User.name</strong> {{User.name}}</div>-->
-    <!--<div class="greeting" v-if="showGreeting">Hi, {{User.name}}!</div>-->
-  </div>
+  <h1>Hello World</h1>
 </div>
 </template>
 
 
 
 <style>
-#app {
+h1 {
   font-family : Arial, sans-serif;
-}
-
-
-.welcome {
-  font-size : 2rem;
-  color : #333;
+  font-size: 6rem;
+  color: #333;
   text-align : center;
-  margin: 4rem auto 2rem auto;
+  margin: 10rem auto;
+  position: relative;
+  top: 50%;
+  bottom: 50%;
   width: 100%;
-}
-
-.welcome_image {
-  height: 12rem;
-  width: 12rem;
-  border-radius: 8rem;
-  margin: 1rem auto;
-  background-image: url('https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=375&w=630');
-  background-size: cover;
-  background-position: 50%;
-  border: 1px solid #a9463a;
-}
-
-.welcome_message {
-
-}
-
-.wrapper {
-  /*border: 1px solid darkred;*/
-}
-
-.form {
-  margin : auto;
-  text-align: center;
-  padding: 1rem;
-}
-
-.form .question {
-  margin: 1rem auto;
-  font-size: 0.8rem;
-}
-
-.form .debug {
-  font-size: 0.8rem;
-  margin: 0.5rem;
-}
-
-.form button {
-  font-size: 0.8rem;
-  padding: 0.5rem 0.8rem;
-  border-radius: 0 0.5rem 0.5rem 0;
-  margin-left: -2.3rem;
-  border: 0px solid #a9463a;
-  background-color: #a9463a;
-  color: white;
-  outline: none;
-  cursor: pointer;
-}
-
-.form input.username {
-  height: 1.4rem;
-  width: 16rem;
-  padding: 0.2rem 0.6rem;
-  font-size: 0.8rem;
-  border-radius: 0.6rem;
-  border: 1px solid #a9463a;
-  outline: none;
-}
-
-.form .greeting {
-  margin-top: 0.8rem;
 }
 </style>
 
