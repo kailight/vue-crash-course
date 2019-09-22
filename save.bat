75d62b937b201f19a24cf09278a2666468488abb
KaiLight <lastgrunger@gmail.com>
2019-09-22 04:24:14 +0300

    Lesson #12 End

--- save.bat
+++
@@ -1,4 +0,0 @@
-git tag -d 12.0
-git add .
-git commit . -m "Lesson #12 Test Result"
-git tag -a 12.0 -m "Lesson #12 Test Result"
--- save.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git tag -d 12.5
-git add .
-git commit . -m "Lesson #12 Test Result"
-git tag -a 12.5 -m "Lesson #12 Test Result"
