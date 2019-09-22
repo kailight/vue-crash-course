b4d6e835daa9ea1a766bd76069ad826485e94f0f
KaiLight <lastgrunger@gmail.com>
2019-09-22 03:43:22 +0300

    Lesson #11 End

--- save.bat
+++
@@ -1,4 +0,0 @@
-git tag -d 11.5
-git add .
-git commit . -m "Lesson #11 Test Result"
-git tag -a 11.5 -m "Lesson #11 Test Result"
--- save.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git tag -d 11.5
-git add .
-git commit . -m "Lesson #11 Test Result"
-git tag -a 11.5 -m "Lesson #11 Test Result"
